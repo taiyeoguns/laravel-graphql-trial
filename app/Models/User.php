4b6f9665a28a66869352a020de6c90f064453637
Taiye <taiyeoguns@yahoo.com>
2019-05-24 21:00:09 +0100

    Add uuid to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Dyrynda\Database\Support\GeneratesUuid;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use GeneratesUuid;
 
     /**
      * The attributes that are mass assignable.
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Ramsey\Uuid\Uuid;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,7 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'uuid'  => Uuid::uuid4()->toString(),
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
