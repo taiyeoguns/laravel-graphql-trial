e6709a2071d6956cb20fc7b85824540544b0125f
Taiye <taiyeoguns@yahoo.com>
2019-06-11 16:15:44 +0100

    Update User model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,10 @@ use Ramsey\Uuid\Uuid;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'uuid'  => Uuid::uuid4()->toString(),
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'first_name' => $fn = $faker->firstName,
+        'last_name' => $ln = $faker->lastName,
+        'email' => strtolower(sprintf('%s.%s@lvgraphql.local', $fn, $ln)),
+        'phone' => $faker->phoneNumber,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
